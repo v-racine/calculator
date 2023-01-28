67af5da8b754e5a9acdca6931463b5c54fefe013
Valerie Racine <vracine21@gmail.com>
2023-01-27 20:15:02 -0500

    refactor: program

--- calculator.js
+++ calculator.js
@@ -6,6 +6,128 @@
 const readline = require("readline-sync");
 const MESSAGES = require("./calculator_messages.json");
 
+// main function
+function startCalculator() {
+  const language = determineLanguage();
+  determineName(language);
+  let anotherCalc = "y";
+  while (anotherCalc === "y") {
+    const number1 = determineNum(language, "num1");
+    const number2 = determineNum(language, "num2");
+    const operator = determineOperator(language);
+    const result = determineResult(language, number1, number2, operator);
+    printResult(language, result);
+    anotherCalc = keepCalculating(language);
+    console.clear();
+  }
+}
+
+startCalculator();
+
+//helper functions
+function determineLanguage() {
+  let intro = "";
+  let language;
+  while (intro === "") {
+    prompt(messages("intro"));
+    intro = readline.question();
+
+    const lowerCasedLang = intro.toLowerCase();
+
+    if (lowerCasedLang === "en") {
+      language = "en";
+    } else if (lowerCasedLang === "fr") {
+      language = "fr";
+    } else {
+      intro = "";
+    }
+  }
+
+  return language;
+}
+
+function determineName(lang) {
+  prompt(messages("welcome", lang));
+  let validName = readline.question();
+
+  while (validName === "") {
+    prompt(messages("invalidName", lang));
+    validName = readline.question();
+  }
+}
+
+function determineNum(lang, msg) {
+  prompt(messages(msg, lang));
+  let num = readline.question();
+
+  while (invalidNumber(num)) {
+    prompt(messages("invalidNumber", lang));
+    num = readline.question();
+  }
+
+  return Number(num);
+}
+
+function invalidNumber(number) {
+  return number.trimStart() === "" || Number.isNaN(Number(number));
+}
+
+function determineOperator(lang) {
+  prompt(messages("operation", lang));
+  let operation = readline.question();
+
+  while (!["1", "2", "3", "4"].includes(operation)) {
+    prompt(messages("invalidOperation", lang));
+    operation = readline.question();
+  }
+
+  return operation;
+}
+
+function determineResult(lang, number1, number2, operator) {
+  let output;
+  switch (operator) {
+    case "1":
+      output = number1 + number2;
+      break;
+    case "2":
+      output = number1 - number2;
+      break;
+    case "3":
+      output = number1 * number2;
+      break;
+    case "4":
+      if (number2 === 0) {
+        output = messages("infinity", lang);
+      } else {
+        output = number1 / number2;
+      }
+      break;
+  }
+  return output;
+}
+
+function printResult(lang, result) {
+  console.log(messages("result", lang), result);
+}
+
+function keepCalculating(lang) {
+  prompt(messages("runProgram", lang));
+  anotherCalc = readline.question();
+
+  while (
+    anotherCalc !== "n" &&
+    anotherCalc !== "N" &&
+    anotherCalc !== "y" &&
+    anotherCalc !== "Y"
+  ) {
+    prompt(messages("invalidChoice", lang));
+    anotherCalc = readline.question();
+  }
+
+  return anotherCalc.toLowerCase();
+}
+
 function prompt(message) {
   console.log(`=> ${message}`);
 }
@@ -14,83 +136,93 @@ function messages(message, lang = "en") {
   return MESSAGES[lang][message];
 }
 
-let intro = "";
-while (intro === "") {
-  prompt(messages("intro"));
-  intro = readline.question();
+//   // First Draft of Program starts here:
 
-  const lowerCasedLang = intro.toLowerCase();
+// function prompt(message) {
+//   console.log(`=> ${message}`);
+// }
 
-  if (lowerCasedLang === "en") {
-    LANGUAGE = "en";
-  } else if (lowerCasedLang === "fr") {
-    LANGUAGE = "fr";
-  } else {
-    intro = "";
-  }
-}
+// function messages(message, lang = "en") {
+//   return MESSAGES[lang][message];
+// }
 
-prompt(messages("welcome", LANGUAGE));
-let validName = readline.question();
+// let intro = "";
+// while (intro === "") {
+//   prompt(messages("intro"));
+//   intro = readline.question();
 
-while (validName === "") {
-  prompt(messages("invalidName", LANGUAGE)); //when user inputs nothing (empty string) or whitespace
-  validName = readline.question();
-}
+//   const lowerCasedLang = intro.toLowerCase();
 
-function invalidNumber(number) {
-  return number.trimStart() === "" || Number.isNaN(Number(number));
-}
+//   if (lowerCasedLang === "en") {
+//     LANGUAGE = "en";
+//   } else if (lowerCasedLang === "fr") {
+//     LANGUAGE = "fr";
+//   } else {
+//     intro = "";
+//   }
+// }
 
-let runProgram = "y";
+// prompt(messages("welcome", LANGUAGE));
+// let validName = readline.question();
 
-while (runProgram === "y" || runProgram === "Y") {
-  prompt(messages("num1", LANGUAGE));
-  let num1 = readline.question();
+// while (validName === "") {
+//   prompt(messages("invalidName", LANGUAGE)); //when user inputs nothing (empty string) or whitespace
+//   validName = readline.question();
+// }
 
-  while (invalidNumber(num1)) {
-    prompt(messages("invalidNumber", LANGUAGE));
-    num1 = readline.question();
-  }
+// function invalidNumber(number) {
+//   return number.trimStart() === "" || Number.isNaN(Number(number));
+// }
 
-  prompt(messages("num2", LANGUAGE));
-  let num2 = readline.question();
+// let runProgram = "y";
 
-  while (invalidNumber(num2)) {
-    prompt(messages("invalidNumber", LANGUAGE));
-    num2 = readline.question();
-  }
+// while (runProgram === "y" || runProgram === "Y") {
+//   prompt(messages("num1", LANGUAGE));
+//   let num1 = readline.question();
 
-  prompt(messages("operation", LANGUAGE));
-  let operation = readline.question();
+//   while (invalidNumber(num1)) {
+//     prompt(messages("invalidNumber", LANGUAGE));
+//     num1 = readline.question();
+//   }
 
-  while (!["1", "2", "3", "4"].includes(operation)) {
-    prompt(messages("invalidOperation", LANGUAGE));
-    operation = readline.question();
-  }
+//   prompt(messages("num2", LANGUAGE));
+//   let num2 = readline.question();
 
-  let output;
-  switch (operation) {
-    case "1":
-      output = Number(num1) + Number(num2);
-      break;
-    case "2":
-      output = Number(num1) - Number(num2);
-      break;
-    case "3":
-      output = Number(num1) * Number(num2);
-      break;
-    case "4":
-      if (Number(num2) === 0) {
-        output = messages("infinity", LANGUAGE);
-      } else {
-        output = Number(num1) / Number(num2);
-        break;
-      }
-  }
+//   while (invalidNumber(num2)) {
+//     prompt(messages("invalidNumber", LANGUAGE));
+//     num2 = readline.question();
+//   }
 
-  console.log(messages("result", LANGUAGE), output);
+//   prompt(messages("operation", LANGUAGE));
+//   let operation = readline.question();
 
-  prompt(messages("runProgram", LANGUAGE));
-  runProgram = readline.question();
-}
+//   while (!["1", "2", "3", "4"].includes(operation)) {
+//     prompt(messages("invalidOperation", LANGUAGE));
+//     operation = readline.question();
+//   }
+
+//   let output;
+//   switch (operation) {
+//     case "1":
+//       output = Number(num1) + Number(num2);
+//       break;
+//     case "2":
+//       output = Number(num1) - Number(num2);
+//       break;
+//     case "3":
+//       output = Number(num1) * Number(num2);
+//       break;
+//     case "4":
+//       if (Number(num2) === 0) {
+//         output = messages("infinity", LANGUAGE);
+//       } else {
+//         output = Number(num1) / Number(num2);
+//         break;
+//       }
+//   }
+
+//   console.log(messages("result", LANGUAGE), output);
+
+//   prompt(messages("runProgram", LANGUAGE));
+//   runProgram = readline.question();
+// }
